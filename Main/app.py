904e533fcd5eb85d5d1d1ffaaeac780d8cd5f7eb
Corn's Mac Book Air <happpy.corn.man@gmail.com>
2025-10-09 12:10:01 +0800

    update

--- Main/app.py
+++ Main/app.py
@@ -55,4 +55,4 @@ def people_count():
 if __name__ == '__main__':
     t = threading.Thread(target=update_loop, daemon=True)
     t.start()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
